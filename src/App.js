d9a437d6b22095ad1bbdae68a5ae755aa61e0922
bhagyamanii <uppalprateek09@gmail.com>
2023-09-26 16:34:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 function App() {
   return (
     <div className="root">
